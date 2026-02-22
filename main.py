58e1f3fa33c61aaf426f11d72328483191f85d05
mdmeherabhossin860-blip <mdmeherabhossin860@gmail.com>
2026-02-22 16:25:06 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 name: Skeleton Build
-
 on: [push]
 
 jobs:
   build:
     runs-on: ubuntu-latest
     steps:
-      - uses: actions/checkout@v3
-      - name: Run a one-line script
-        run: echo Hello, world!
+      - uses: actions/checkout@v4
+      
+      - name: Build APK
+        uses: ArtemSerebrennikov/buildozer-action@v1
+        with:
+          buildozer_version: stable
+          command: buildozer android debug
+
+      - name: Upload APK
+        uses: actions/upload-artifact@v4
+        with:
+          name: MyEmptyApp
+          path: bin/*.apk
